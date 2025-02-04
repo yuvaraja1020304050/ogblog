4dec2d3f787e6cd1430a97d5fddb92ab8b4bab3e
Yuvaraja Vani Ramesh <yuvaraja.r@jmangroup.com>
2025-02-04 18:21:11 +0530

    added extra

--- vite-project/src/Pages/Create.jsx
+++ vite-project/src/Pages/Create.jsx
@@ -3,9 +3,35 @@ import React from 'react'
 const Create = () => {
   return (
    
-         <div className='h-140 border-2 w-130 mx-100 my-40'>create
-         <input type="text" placeholder="enter the title"/>
-         <input type="text" placeholder="enter the title"/>
+         <div className='h-140 border-3 w-130 mx-120 my-40 '>
+         <div className='flex flex-col gap-6 mt-2'>
+          <h1 className='font-bold text-blue-800 ml-40 underline'>Create your own blog</h1>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+          <div className='flex gap-6 ml-2'>
+          <h3 >Enter title:</h3>
+          <input className='border-2 rounded-full w-90' type="text" placeholder="    enter the title"/>
+          </div>
+         </div>
+         
          </div>
   
    
